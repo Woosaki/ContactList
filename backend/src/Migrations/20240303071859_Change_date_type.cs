42e7e4ebe82a6e66bbc6e99e64fe0d5b0dc58544
Woosaki <kamil.szczukowski@interia.pl>
2024-03-03 08:23:28 +0100

    Change type to date

---
+++ backend/src/Migrations/20240303071859_Change_date_type.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactListAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Change_date_type : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Birthdate",
+                table: "Contacts",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Birthdate",
+                table: "Contacts",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+        }
+    }
+}
